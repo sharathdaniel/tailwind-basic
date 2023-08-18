d2119ad720e5ddf70bc700f90a3de5639dd5104c
Sharath Daniel <5154064+sharathdaniel@users.noreply.github.com>
2023-08-18 13:38:15 +0530

    custom colors, layout container settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["*.html"],
   darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '0.9375rem' /* 15px */
+    },
+    extend: {
+      colors: {
+        black: '#222',
+        gray: {
+          100: '#f7fafc',
+          900: '#1a202c'
+        }
+      }
+    },
   },
   plugins: [],
 }
